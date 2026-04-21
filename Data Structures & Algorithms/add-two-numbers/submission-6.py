49f216502ffa7bd2ab78d988b245771dee5c7a14
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-04-21 22:25:37 +0900

    Add: add-two-numbers - submission-6

---
+++ Data Structures & Algorithms/add-two-numbers/submission-6.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail, h1, h2 = dummy, l1, l2
+        carry = 0
+
+        while h1 or h2 or carry:
+            v1 = h1.val if h1 else 0
+            v2 = h2.val if h2 else 0
+            val = v1 + v2 + carry
+
+            digit = val % 10
+            carry = val // 10
+
+            tail.next = ListNode(digit)
+            tail = tail.next
+            h1 = h1.next if h1 else None
+            h2 = h2.next if h2 else None
+        
+        return dummy.next
