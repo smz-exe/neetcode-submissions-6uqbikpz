09b57e5f63d6443cfad82bfb8d08f012ed3334e2
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-05-24 20:43:22 +0900

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+        
+        rows, cols = len(heights), len(heights[0])
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        pac, atl = set(), set()
+
+        def dfs(r: int, c: int, visited: set[tuple(int, int)], prev_height: int):
+            out_of_bounds = r < 0 or c < 0 or r >= rows or c >= cols
+            if (
+                out_of_bounds
+                or (r, c) in visited 
+                or heights[r][c] < prev_height
+            ):
+                return
+            
+            visited.add((r, c))
+            for dr, dc in directions:
+                dfs(r + dr, c + dc, visited, heights[r][c])
+
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
+        
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
+        
+        res = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+
+        return res
+        
