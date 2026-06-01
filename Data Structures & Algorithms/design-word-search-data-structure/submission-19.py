31e55dc490cbd9e0c463ed3d42c4ac9683cc9e90
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-06-01 18:38:46 +0900

    Add: design-word-search-data-structure - submission-19

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-19.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.is_word = False
+
+class WordDictionary:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        
+        cur.is_word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(start: int, node: TrieNode) -> bool:
+            for idx in range(start, len(word)):
+                c = word[idx]
+   
+                if c == ".":
+                    for child in node.children.values():
+                        if dfs(idx + 1, child):
+                            return True
+
+                    return False
+                else:
+                    if c not in node.children:
+                        return False
+                    node = node.children[c]
+            
+            return node.is_word
+        
+        return dfs(0, self.root)
+                
+
+
+                
+
