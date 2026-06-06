4eb21ea089abf33e880a0016c067535f2777a9f4
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-06-06 21:49:01 +0900

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,32 @@
+import heapq
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visited = set()
+        min_heap = [(grid[0][0], 0, 0)] # (time/max_height, r, c)
+        visited.add((0, 0))
+
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        while min_heap:
+            t, r, c  = heapq.heappop(min_heap)
+
+            if (r, c) == (n - 1 , n - 1):
+                return t
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                out_of_bounds = nr < 0 or nc < 0 or nr >= n or nc >= n
+
+                if (
+                    out_of_bounds
+                    or (nr, nc) in visited
+                ):
+                    continue
+                
+                visited.add((nr, nc))
+                heapq.heappush(min_heap, (max(t, grid[nr][nc]), nr, nc))
+        
+
+
