8fa2b7547be60f2784595dc08c1e15226a3bcde5
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-06-08 20:09:04 +0900

    Add: search-for-word-ii - submission-14

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-14.py
@@ -0,0 +1,63 @@
+# I built a Trie from the word list, then start DFS only from cells that match a root child.
+# During DFS, I only recurse into neighboring cells if they are in bounds, unvisited, and match a child of the current Trie node.
+# So the Trie let us prune  paths as soon as they stop being a prefix of any word.
+# When I find a complete word, I add it to the result and set node.word to None to avoid duplicates.
+
+class TrieNode:
+    def __init__(self) -> None:
+        self.children: dict[str, "TrieNode"] = {}
+        self.word: str | None = None
+    
+    def add_word(self, word: str) -> None:
+        cur = self
+
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        
+        cur.word = word
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not board or not board[0]:
+            return []
+        
+        rows, cols = len(board), len(board[0])
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        visited = set()
+        res = []
+
+        root = TrieNode()
+        for word in words:
+            root.add_word(word)
+        
+        def dfs(r: int, c: int, node: TrieNode) -> None:
+            char = board[r][c]
+            node = node.children[char]
+            visited.add((r, c))
+
+            if node.word:
+                res.append(node.word)
+                node.word = None
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if (
+                    0 <= nr < rows
+                    and 0 <= nc < cols
+                    and (nr, nc) not in visited
+                    and board[nr][nc] in node.children
+                ):
+                    dfs(nr, nc, node)
+            
+            visited.remove((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] in root.children:
+                    dfs(r, c, root)
+        
+        return res
