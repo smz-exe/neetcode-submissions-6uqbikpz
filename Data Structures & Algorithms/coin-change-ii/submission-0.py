716400c1e2309712a017e5d4ea8809d5b1ed6473
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-06-29 18:28:19 +0900

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        cache = {}
+
+        def dfs(i: int, a: int) -> int:
+            if a == amount:
+                return 1
+            
+            if a > amount:
+                return 0
+            
+            if i == len(coins):
+                return 0
+            
+            if (i, a) in cache:
+                return cache[(i, a)]
+            
+            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            return cache[(i, a)]
+        
+        return dfs(0, 0)
