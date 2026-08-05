480b5d89389ecc7abcbee90790880e73dbd9794d
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-08-05 17:40:18 +0900

    Add: multiply-strings - submission-2

---
+++ Data Structures & Algorithms/multiply-strings/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        n, m = len(num1), len(num2)
+
+        acc = [0] * (n + m)
+
+        for i in range(n):
+            digit1 = ord(num1[n - 1 - i]) - ord("0")
+
+            for j in range(m):
+                digit2 = ord(num2[m - 1 - j]) - ord("0")
+                acc[i + j] += digit1 * digit2
+                print(digit1, digit2, acc[i + j])
+        
+        for k in range(n + m - 1):
+            acc[k + 1] += acc[k] // 10
+            acc[k] %= 10
+        
+        while len(acc) > 1 and acc[-1] == 0:
+            acc.pop()
+        
+        return "".join(chr(digit + ord("0")) for digit in reversed(acc))
