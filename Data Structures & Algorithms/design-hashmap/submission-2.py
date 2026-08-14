69f56a6fc765c02e4e4dd314632bcb8ec3cb8f61
Yuki Shimizu <142222649+smz-exe@users.noreply.github.com>
2026-08-14 16:50:24 +0900

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.py
@@ -0,0 +1,54 @@
+class ListNode:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.next: ListNode | None = None
+
+class MyHashMap:
+    BUCKET_COUNT = 1000
+
+    def __init__(self):
+        self.buckets = [ListNode(-1, -1) for _ in range(self.BUCKET_COUNT)]
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.BUCKET_COUNT
+        cur = self.buckets[index]
+
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.value = value
+                return
+
+            cur = cur.next
+        
+        cur.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        index = key % self.BUCKET_COUNT
+        cur = self.buckets[index]
+
+        while cur.next:
+            if cur.next.key == key:
+                return cur.next.value
+            
+            cur = cur.next
+        
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.BUCKET_COUNT
+        cur = self.buckets[index]
+
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            
+            cur = cur.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
